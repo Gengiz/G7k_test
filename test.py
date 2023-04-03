29f3781b4df7569c5b76c77893996673534aaa75
Gengiz <127804016+Gengiz@users.noreply.github.com>
2023-04-03 01:43:00 +0100

    update test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+This tool is an experiment
+print ("This tool is an experiment")
+
+print ("Genghis Khan welcomes you to the Wi>
+
+import os
+#os.system ('clear')
+list123 = 10
+while list123 < 9 :
+    list123=list123+1
+    print (list123)
